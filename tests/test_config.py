b251198fbcebafd206d510911da09c53acb78e3e
Dave St.Germain <dstgermain@edx.org>
2014-06-09 11:21:26 -0400

    Trying to get tests working.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,7 @@
 import os
 from unittest import TestCase
 
-from i18n.config import Configuration, LOCALE_DIR, CONFIGURATION
+from i18n import config
 
 class TestConfiguration(TestCase):
     """
@@ -9,14 +9,14 @@ class TestConfiguration(TestCase):
     """
 
     def test_config(self):
-        config_filename = os.path.normpath(os.path.join(LOCALE_DIR, 'config.yaml'))
-        config = Configuration(config_filename)
-        self.assertEqual(config.source_locale, 'en')
+        config_filename = os.path.normpath(os.path.join('tests', 'data', 'config.yaml'))
+        cfg = config.Configuration(config_filename)
+        self.assertEqual(cfg.source_locale, 'en')
 
     def test_no_config(self):
-        config_filename = os.path.normpath(os.path.join(LOCALE_DIR, 'no_such_file'))
+        config_filename = os.path.normpath(os.path.join(config.LOCALE_DIR, 'no_such_file'))
         with self.assertRaises(Exception):
-            Configuration(config_filename)
+            config.Configuration(config_filename)
 
     def test_valid_configuration(self):
         """
@@ -24,10 +24,14 @@ class TestConfiguration(TestCase):
         and that it contains an 'en' locale.
         Also check values of dummy_locale and source_locale.
         """
-        self.assertIsNotNone(CONFIGURATION)
-        locales = CONFIGURATION.locales
+        self.assertIsNotNone(config.CONFIGURATION)
+        # these will just be defaults
+        locales = config.CONFIGURATION.locales
         self.assertIsNotNone(locales)
         self.assertIsInstance(locales, list)
+        config_filename = os.path.normpath(os.path.join('tests', 'data', 'config.yaml'))
+        config.CONFIGURATION = config.Configuration(config_filename)
+
         self.assertIn('en', locales)
-        self.assertEqual('eo', CONFIGURATION.dummy_locales[0])
-        self.assertEqual('en', CONFIGURATION.source_locale)
+        self.assertEqual('eo', config.CONFIGURATION.dummy_locales[0])
+        self.assertEqual('en', config.CONFIGURATION.source_locale)
