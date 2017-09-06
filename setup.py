70d18dc7efbaaf85a2dc50122188b6b06087cb65
bmedx <bmesick@edx.org>
2017-09-06 13:25:05 -0400

    Bump version, update README & AUTHORS.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='edx-i18n-tools',
-    version='0.3.9',
+    version='0.3.10',
     description='edX Internationalization Tools',
     author='edX',
     author_email='oscm@edx.org',
