dd0d1375eace51864aa21b5331dc9fc270d24345
Jawayria <jawayriahashmi@gmail.com>
2021-08-20 17:48:06 +0500

    log error

--- i18n/execute.py
+++ i18n/execute.py
@@ -18,7 +18,11 @@ def execute(command, working_directory=config.BASE_DIR, stderr=sp.STDOUT):
     """
     LOG.info("Executing in %s ...", working_directory)
     LOG.info(command)
-    sp.check_call(command, cwd=working_directory, stderr=stderr, shell=True)
+    try:
+        sp.check_call(command, cwd=working_directory, stderr=stderr, shell=True)
+    except sp.CalledProcessError as e:
+        LOG.error(e.output)
+        LOG.error(e.returncode)
 
 
 def call(command, working_directory=config.BASE_DIR):
